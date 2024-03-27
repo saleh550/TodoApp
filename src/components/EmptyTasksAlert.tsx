543180ab54140069969e0e186ea0abd4c8661a23
סאלח פארס <75793688+saleh550@users.noreply.github.com>
2024-03-27 16:04:15 +0200

    empty tasks alert updated

---
+++ src/components/EmptyTasksAlert.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+const EmptyTasksAlert:FC = () => {
+  return (
+    <div>
+      You have nothing to do
+    </div>
+  )
+}
+
+export default EmptyTasksAlert
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,20 +1,23 @@
 import { FC } from "react";
 import { useTodosStore } from "../store";
 import TodoItem from "./TodoItem";
+import EmptyTasksAlert from "./EmptyTasksAlert";
 
 const TodoList: FC = () => {
   const { todos } = useTodosStore((state) => state);
 
   return (
     <div>
-      {todos.length > 0 &&
+      {todos.length > 0 ?
         todos.map((item) => (
           <>
             <div className="py-2" key={item.id}>
               <TodoItem key={item.id} item={item} />
             </div>
           </>
-        ))}
+        )):(
+          <EmptyTasksAlert/>
+        )}
     </div>
   );
 };
